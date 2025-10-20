69d377f9654c6adac3dfce6caa59bbbfbd8d9882
Andrey Chichkov <ferdinand100400@yandex.ru>
2025-10-20 21:31:33 +0300

    Исправление Task 9.1

--- src/main/java/ru/chichkov/ConfigurationString/Configurations.java
+++ src/main/java/ru/chichkov/ConfigurationString/Configurations.java
@@ -3,6 +3,7 @@ package ru.chichkov.ConfigurationString;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 import java.util.Date;
@@ -12,34 +13,35 @@ import java.util.function.Predicate;
 @Configuration
 public class Configurations {
 
+    // Задача 9.1.1
     @Bean
     public String helloWorld() {
         return "Hello world";
     }
-
+    // Задача 9.1.2
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)   // Каждый раз при обращении создаем новый бин, можно так: @Scope("prototype")
     public int random() {
         Random random = new Random();
         return random.nextInt(100);
     }
-
+    // Задача 9.1.3
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @Lazy    // Создаем бин когда потребуется, по умолчанию он создается при создании контекста
     public Date start() {
         return new Date();
     }
-
+    // Задача 9.1.4
     @Bean
     public Predicate<Integer> range() {
         return x -> (x >= 2 && x <= 5);
     }
-
+    // Задача 9.1.5
     @Bean
     public int max() {
         return 100;
     }
-
+    // Задача 9.1.5
     @Bean
     public int min() {
         return 0;
